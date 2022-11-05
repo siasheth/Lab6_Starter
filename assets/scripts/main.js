512808b067bd9cc87271527cfed68c29aaab0bec
Sia Sheth <snsheth@ucsd.edu>
2022-11-05 15:42:21 -0700

    expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,13 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  const storage = JSON.parse(localStorage.getItem('recipes'));
+  if (storage == null) {
+    return [];
+  } 
+  else {
+    return storage;
+  }
 }
 
 /**
@@ -35,10 +42,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  const main = document.querySelector('main');
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  for (const recipe of recipes) {
+    const card = document.createElement('recipe-card');
+    card.data = recipe;
+    main.append(card);
+  }
 }
 
 /**
@@ -51,6 +64,7 @@ function saveRecipesToStorage(recipes) {
   // B1. TODO - Complete the functionality as described in this function
   //            header. It is possible in only a single line, but should
   //            be no more than a few lines.
+  storage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
@@ -60,6 +74,7 @@ function saveRecipesToStorage(recipes) {
 function initFormHandler() {
 
   // B2. TODO - Get a reference to the <form> element
+  
   
   // B3. TODO - Add an event listener for the 'submit' event, which fires when the
   //            submit button is clicked
